36849a3f9fdad9fff71a781b00081dd171b6de61
piyush_gupta <ypiyushgupta@gmail.com>
2024-01-10 23:58:06 +0530

    Trailer

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
+import addTrailerVideo from "./moviesSlice";
 
 const appStore = configureStore(
     {
